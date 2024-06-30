94929c91c69aef281054c9bd39f5445b542e4f17
nehapansare <nehapansare007@gmail.com>
2024-06-30 19:36:46 +0530

    image file added

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function pro_img(num)
   const proimg = document.getElementById('product');
   
   if(num==1){
-    proimg.src ="1image.jpg";
+    proimg.src ="./product images/1image.jpg";
   }
   else if(num==2)
     {
-        proimg.src="2image.jpg";
+        proimg.src="./product images/2image.jpg";
     }
   else if(num==3)
         {
-            proimg.src="3image.jpg";
+            proimg.src="./product images/3image.jpg";
         }  
         else if(num==4)
             {
-                proimg.src="4image.jpg";
+                proimg.src="./product images/4image.jpg";
             }     
 }
